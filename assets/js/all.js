e7922031427a12a753e3ff7dffa55334822201eb
Sazzad171 <sajjad.cse.diu@gmail.com>
2020-09-01 18:16:56 +0600

    category page updated

--- assets/js/all.js
+++ assets/js/all.js
@@ -21,3 +21,22 @@ function hideSearchBar() {
   }, function() {
     $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(100);
   });
+
+  // form validation with js
+  (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
